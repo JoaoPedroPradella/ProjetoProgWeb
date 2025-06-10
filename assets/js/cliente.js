cbe04225cbca1f30b197bef579552dae385b579d
João Pedro Gossenheimer Pradella <160424478+JoaoPedroPradella@users.noreply.github.com>
2025-06-09 22:11:28 -0300

    Create cliente.js

---
+++ assets/js/cliente.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector("dialog#modal")
+
+function listarClientes() {
+    let tipo = 'listagem';
+
+    $.ajax({
+        url: 'src/Application/selecionar_cliente.php',
+        method: 'POST',
+        data: {
+            'listagem': tipo
+        },
+        dataType: 'json'
+    }).done(function (result) {
+        tipo = '';
+        console.log(result);
+        if (!result.erro) {
+            $('#lista').empty();
+
+            for (var i = 0; i < result.length; i++) {
+                $('#lista').prepend(
+                    '<tr><td>' + result[i].cd_cliente + '</td><td>' + result[i].ds_nome + '</td></tr>');
+            }
+        } else {
+            alert(result.erro);
+        }
+    });
+}
+// Carrega a listagem de clientes pela primeira vez
+listarClientes();
