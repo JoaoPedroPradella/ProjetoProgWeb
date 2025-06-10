2809893ec1ff1364c71d3f01af7490cb17d9bf61
João Pedro Gossenheimer Pradella <160424478+JoaoPedroPradella@users.noreply.github.com>
2025-06-10 16:35:51 -0300

    Update cad_produto.php

--- cad_produto.php
+++ cad_produto.php
@@ -1,15 +1,15 @@
-<?php include 'sistema.php';?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width">
     <title>Document</title>
     <link rel="stylesheet" href="assets\css\produto.css">
 </head>
 <body>
-    teste
     <main>
+    teste
     </main>
 </body>
 </html>
