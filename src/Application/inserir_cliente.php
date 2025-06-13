3d21e41ffd2908ad1e8b0e25ef792f91dd82838e
João Pedro Gossenheimer Pradella <160424478+JoaoPedroPradella@users.noreply.github.com>
2025-06-13 11:59:40 -0300

    Update inserir_cliente.php

--- src/Application/inserir_cliente.php
+++ src/Application/inserir_cliente.php
@@ -4,7 +4,30 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+use App\Models\BancoDeDados;
 use App\Models\Cliente;
 
+$form['id'] = isset($_POST['id']) ? $_POST['id'] : '';
+$form['nome'] = isset($_POST['nome']) ? $_POST['nome'] : '';
+$form['cpf_cnpj'] = isset($_POST['cpf_cnpj']) ? $_POST['cpf_cnpj'] : '';
+$form['telefone'] = isset($_POST['telefone']) ? $_POST['telefone'] : '';
+$form['cep'] = isset($_POST['cep']) ? $_POST['cep'] : '';
+$form['uf'] = isset($_POST['uf']) ? $_POST['uf'] : '';
+$form['munic'] = isset($_POST['munic']) ? $_POST['munic'] : '';
+$form['lograd'] = isset($_POST['lograd']) ? $_POST['lograd'] : '';
 
-echo json_encode(var_dump($_POST));
+
+if (in_array('', $form)) {
+    echo json_encode('Existem campos vazios. Verifique!');
+}
+
+$bd = new BancoDeDados();
+$novoCliente = new cliente($bd);
+
+
+try {
+    echo json_encode($novoCliente->cadastrar($form));
+
+} catch (Exception $e) {
+    echo json_encode (['erro' => $e -> getMessage()]);
+}
