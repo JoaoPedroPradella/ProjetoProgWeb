a30e5ec2e3fad07a29c5c2a701e7fa4a1d95eb1c
João Pedro Gossenheimer Pradella <160424478+JoaoPedroPradella@users.noreply.github.com>
2025-06-13 11:59:25 -0300

    Update cliente.js

--- assets/js/cliente.js
+++ assets/js/cliente.js
@@ -1,7 +1,9 @@
-const modal = document.querySelector("dialog#modal")
+const modal = document.getElementById('modal');
 
 function listarClientes() {
     let tipo = 'listagem';
+
+    if ($('#conteudo').data('loaded') === 'cliente') return;
 
     $.ajax({
         url: 'src/Application/selecionar_cliente.php',
@@ -12,26 +14,59 @@ function listarClientes() {
         dataType: 'json'
     }).done(function (result) {
         tipo = '';
-        console.log(result);
         if (!result.erro) {
             $('#lista').empty();
 
             for (var i = 0; i < result.length; i++) {
                 $('#lista').prepend(
-                    '<div class="list-group"> <label class="list-group-item d-flex gap-2"> <input class="form-check-input flex-shrink-0" type="radio" name="listGroupRadios" id="listGroupRadios1" value="" checked> <span>' + result[i].ds_nome + ' <small class="d-block text-body-secondary">' + result[i].cd_cliente + '</small></span> </label></div>');
+                    '<div class="list-group"> <label class="list-group-item d-flex gap-2"> <input class="form-check-input flex-shrink-0" type="radio" name="opt_cliente" id="opt_cliente" value="' + result[i].cd_cliente + '" checked> <span>' + result[i].ds_nome + ' <small class="d-block text-body-secondary">' + result[i].cd_cliente + '</small></span> </label></div>');
             }
         } else {
-            alert(result.erro);
+            console.log(result.erro);
         }
     });
 }
 
 
 $("#btn_novo").click(function(){
-    console.log('teste');
     modal.showModal();
+    $('#txttitulo').html('Novo');
     $('#status').hide();
 })
+
+$("#btn_edit").click(function alterarCadastro (){
+    modal.showModal();
+    $('#txttitulo').html('Editando');
+    $('#status').show();
+    $status = $('#checkbox:checked').val();
+    console.log($status);
+
+    $.ajax ({
+        url: 'src/Application/selecionar_cliente.php',
+        method: 'POST',
+        data: {
+            'id': $('#opt_cliente:checked').val(),
+        },
+        dataType: 'json'
+    }).done(function (result){
+        if (!result.erro){
+            $('#txtid').val(result['cd_cliente']);
+            $('#txtnome').val(result['ds_nome'])
+            $('#txtcpf_cnpj').val(result['ds_cpf_cnpj'])
+            $('#txttel').val(result['ds_tel'])
+            $('#txtcep').val(result['ds_cep'])
+            $('#txtuf').val(result['ds_uf'])
+            $('#txtmunic').val(result['ds_municipio'])
+            $('#txtlog').val(result['ds_logradouro'])
+
+            if (result['tp_tipo'] )
+            listarClientes();
+        } else {
+            console.log(result.erro);
+        }
+        
+    });
+});
 
 $('#btn_fechar').click(function(){
     modal.close();
@@ -43,7 +78,7 @@ $('#form').submit(function (e){
         url: 'src/Application/inserir_cliente.php',
         method: 'POST',
         data: {
-            'codigo': $('#txtid').val(),
+            'id': $('#txtid').val(),
             'nome': $('#txtnome').val(),
             'cpf_cnpj': $('#txtcpf_cnpj').val(),
             'telefone': $('#txttel').val(),
@@ -54,9 +89,15 @@ $('#form').submit(function (e){
         },
         dataType: 'json'
     }).done(function (result){
-       console.log(result);
+        modal.close();
+        if (!result.erro) {
+            alert(result);
+            listarClientes();
+        } else {
+            alert('Existem erros no cadastro! Verifique')
+            console.log(result.erro);
+        }
     });
 });
 
-// Carrega a listagem de clientes pela primeira vez
 listarClientes();
